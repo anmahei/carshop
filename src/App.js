a44eb9acb6b35a1df025ec69ebd2112a3014b089
anmahei <anne-mari.heiniola@myy.haaga-helia.fi>
2020-12-04 22:29:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Carlist from './components/Carlist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar position="static" color="secondary">
+        <Toolbar>
+         
+          <Typography variant="h6" >
+            Empun autokauppa
+          </Typography>
+         
+        </Toolbar>
+      </AppBar>
+     <Carlist />
+    </div>
+  );
+}
+
+export default App;
